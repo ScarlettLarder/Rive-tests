7677639fe37e3c6616ded8afce62e10d24f51cc5
ScarlettLarder <alarder@student.strode-college.ac.uk>
2023-07-19 22:34:11 +0100

    MASSIVE DAY FOR RIVERS

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,28 @@
-import Image from 'next/image'
+"use client"
+import { useRive, useStateMachineInput } from "@rive-app/react-canvas";
 
 export default function Home() {
+  const STATE_MACHINE_NAME = "form_in_progress";
+  const { RiveComponent, rive } = useRive({
+    src: "robotV1.riv",
+    stateMachines: "State Machine 1",
+    autoplay: true
+  });
+///https://codesandbox.io/p/sandbox/unruffled-mendel-rszrz?file=%2Fpages%2Findex.js%3A1%2C1-50%2C1
   return (
-    <main className="">
-      <div className="">
-        <p>dwldadkooow</p>
-      </div>
-    </main>
-  )
+    <div>
+      <main>
+        <p>dwadwa</p>
+        <div
+          style={{
+            height: "30vh",
+            width: "30vw",
+          }}
+        >
+          <RiveComponent />
+        </div>
+        <p>dwadwa</p>
+      </main>
+    </div>
+  );
 }
